f38ee60feaaadc50f4481d308279dbd7b7bc788c
Marco Sciuto <12072012+marco-sciuto@users.noreply.github.com>
2021-12-06 15:31:30 +0100

    Day 6

---
+++ day6/Program.cs
@@ -0,0 +1,19 @@
+﻿var lanternfishes = new long[9];
+var input = File.ReadAllText("puzzle").Split(',').Select(n => int.Parse(n));
+foreach (var timer in input)
+{
+    lanternfishes[timer]++;
+}
+
+for (var day = 0; day < 256; day++)
+{
+    var newFishes = lanternfishes[0];
+    for (int i = 1; i < lanternfishes.Length; i++)
+    {
+        lanternfishes[i-1] = lanternfishes[i];
+    }
+    lanternfishes[6] += newFishes;
+    lanternfishes[8] = newFishes;
+}
+
+Console.WriteLine($"Fishes: {lanternfishes.Sum()}");
